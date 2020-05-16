83ba029c1a8d8b6fcab645d0b127953da8fc1249
hassaine <hassaine24@gmail.com>
2020-05-16 23:24:07 +0100

    make BPSO work

--- dataMining/pso/Particule.py
+++ dataMining/pso/Particule.py
@@ -1,22 +1,31 @@
 import numpy as np
 from random import randint
-# seed random number generator
+import math
 
-np.random.seed(42)
+
 class Particule:
+
     def __init__(self,n,v_max):
         self.size=n
-        self.position = np.random.choice(2,n,p=[0.98, 0.02])
+        self.position =  np.random.choice(2,n,p=[0.98, 0.02]) # scince 2% of our data is 1 in the dataset this can depend on the data
+        # if you are using some small dataset with a lot of 1 then you may use  diffrent parameter for the p
         self.pBest = self.position
         self.Vid = randint(0,v_max)
+        self.pbestFitness=0
 
-    def fitness(self,df,postion=True):
+    def fitness(self,df,postion=True,lift=False):
+        # if the particule is a valide  X=>Y  following the condification
         if(self.validParticule()):
             supportRegle = np.array([False for _ in range(df.shape[0])])
             supportPremis = np.array([False for _ in range(df.shape[0])])
+            if(lift):
+                supportConclusion = np.array([False for _ in range(df.shape[0])])
+
             enter = False
+            # this conditon is used when we try to get the fitness of new postion
             if(postion):
                 particule=self.position
+            # if we want to get the fitness fo the pbest position
             else:
                 particule=self.pBest
             for i in range(0, self.size, 2):
@@ -30,6 +39,13 @@ class Particule:
                         else:
                             # for calculation sup(X)  in X=>Y
                             supportPremis = supportPremis & np.array(df.iloc[:, index] == 1)
+                    elif (lift):
+                        if (not enter):
+                            # for calculation sup(Y)  in X=>Y
+                            supportConclusion = np.array(df.iloc[:, index] == 1)
+                        else:
+                            # for calculation sup(Y)  in X=>Y
+                            supportConclusion = supportPremis & np.array(df.iloc[:, index] == 1)
 
                     # enter the if (particule[i]==1) for the first time
                     if (not enter):
@@ -42,17 +58,37 @@ class Particule:
             supportRegle=(supportRegle==True).sum()
             supportPremis=(supportPremis==True).sum()
             if(supportPremis!=0 and supportRegle!=0):
-                return (supportRegle/supportPremis)#*supportRegle
+                #if the lift is chosen as the fitness musure
+                if(lift):
+                    if(supportConclusion!=0):
+                        return (supportRegle/(supportPremis*supportConclusion))
+                    else:
+                        return 0
+
+                #else we use the support as default
+                else:
+                    return (supportRegle/supportPremis)#*supportRegle
             else:
                 return 0
         else:
             return 0
 
+    def updatePosition(self,df):
+        bestScore = self.pbestFitness
+        i=0
 
+        while(i<self.Vid):
+          indice = np.random.randint(self.size)
+          self.position[indice]=int(math.fabs(self.position[indice]-1))
+          newScore=self.fitness(df)
+          if(newScore - bestScore < 0):
+              self.position[indice] = int(math.fabs(self.position[indice] - 1))
 
-    def updatePosition(self): pass
+          else:
+              self.pBest=self.position
+              self.pbestFitness=newScore
+          i+=1
 
-    def updateVelocity(self): pass
 
     def getRule(self,columns):
         premis = []
@@ -87,6 +123,11 @@ class Particule:
             return True
         else:
             return False
+
+    def equal(self,particule):
+        return ((self.pBest == particule.pBest) == True).sum() == self.size
+
+
 
 
 
